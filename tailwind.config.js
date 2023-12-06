be6a1686464d5c01b05e19ca8b485472441c37bc
Ayush Dabral <ayush.dabral02@gmail.com>
2023-12-06 22:32:37 +0530

    Completed the netflix homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       backgroundImage: {
         'header-img' : "url('https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_small.jpg')"
-      }
+      },
+      screens: {
+        'nav-sec':'274px',
+        'nav-brk':'600px',
+        'mobile': '900px',
+        // => @media (min-width: 992px) { ... }
+      },
     },
   },
   plugins: [],
